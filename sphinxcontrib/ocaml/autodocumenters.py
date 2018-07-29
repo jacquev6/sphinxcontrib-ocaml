69905639361d4870eaf9fceb599868c19215dd3d
Robin Björklin <robin.bjorklin@gmail.com>
2018-07-29 22:15:35 +0200

    make module_name to file_name be case sensitive
    * Imported logger
    * Raise SphinxError instead of exit(1)

--- sphinxcontrib/ocaml/autodocumenters.py
+++ sphinxcontrib/ocaml/autodocumenters.py
@@ -3,10 +3,15 @@
 # Copyright 2017 Vincent Jacques <vincent@vincent-jacques.net>
 
 import json
+import os
 import os.path
 import subprocess
 
 import sphinx.ext.autodoc
+from sphinx.errors import SphinxError
+from sphinx.util import logging
+
+logger = logging.getLogger(__name__)
 
 
 class Generator:
@@ -156,19 +161,17 @@ class ModuleDocumenter(sphinx.ext.autodoc.Documenter):
 
     def generate(self, more_content=None, real_modname=None, check_module=False, all_members=False):
         if self.env.config.ocaml_source_directories is None:
-            # @todo Use Sphinx's warnings and errors infrastructure
-            print("ERROR: please set ocaml_source_directories in conf.py")
-            exit(1)
+            raise SphinxError('Please set ocaml_source_directories in conf.py')
 
         module_name = self.directive.arguments[0]
         for d in self.env.config.ocaml_source_directories:
-            interface_file_name = "{}/{}.mli".format(d, module_name)
-            if os.path.isfile(interface_file_name):
+            file_name = [f for f in os.listdir(d) if f.lower() == "{}.mli".format(module_name.lower())][0]
+            interface_path = os.path.join(d, file_name)
+            if os.path.isfile(interface_path):
                 break
         else:
-            # @todo Use Sphinx's warnings and errors infrastructure
-            print("ERROR: {}.mli not found in any ocaml_source_directories".format(module_name))
-            exit(1)
+            raise SphinxError('%s.mli not found in any ocaml_source_directories' %
+                            module_name)
 
         includes = self.env.config.ocaml_include_directories + [
             subprocess.run(
@@ -181,7 +184,7 @@ class ModuleDocumenter(sphinx.ext.autodoc.Documenter):
         ]
 
         contents = json.loads(subprocess.run(
-            [self.env.config.ocaml_autodoc_executable, interface_file_name] + includes,
+            [self.env.config.ocaml_autodoc_executable, interface_path] + includes,
             stdout=subprocess.PIPE,
             check=True,
             universal_newlines=True,
